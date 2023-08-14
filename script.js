1791a780464c9a69ff1ef551a92809b0c8d5e79b
lenny dzatsi <lennydzatsi8@gmail.com>
2023-08-14 17:53:58 +0200

    add some events for reset

--- script.js
+++ script.js
@@ -6,22 +6,30 @@
 // Récuperation des élement html
 const input = document.getElementById('input');
 const calcul = document.getElementById('calcul');
-const buttons = document.querySelectorAll('button');
+// const buttons = document.querySelectorAll('button');
 const resetBtn = document.getElementById('reset');
 const clearBtn = document.getElementById('clear');
 const equalsBtn = document.getElementById('equals');
-const keys = 
-
-
-// add events
+const plusBtn = document.getElementById('plus'); 
+const buttons = document.querySelectorAll('.numpad');
+const minusBtn = document.getElementById('minus');
+const timesBtn = document.getElementById('times');
+const dividebyBtn = document.getElementById('divideby');
+const plusmoins = document.getElementById("plusoumoins");
+const percentage = document.getElementById('percentage');
+ 
 buttons.forEach(button => {
     button.addEventListener("click", () => {
         input.value += button.textContent;
     })
 });
-resetBtn.addEventListener('click', () => {
-    resetCalculator();
-})
+
+resetBtn.addEventListener('click', (event) =>{
+    event.preventDefault();
+    input.value = ''; 
+});
+
+
 
 
 
