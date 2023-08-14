55ce96989b15917fa9ca75cff31a43c8f92da4c9
lenny dzatsi <lennydzatsi8@gmail.com>
2023-08-14 09:36:34 +0200

    add dat key to html and identifie in js

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
-{ calculate } import './calculator';
+// { calculate } import { Console } from 'console';
+// import './calculator';
 
 // TODO: Faire la manipulation du DOM dans ce fichier
-// Récuperation des élement html
 
+// Récuperation des élement html
 const input = document.getElementById('input');
 const calcul = document.getElementById('calcul');
-const buttons = document.querySelectorAll('#percentage', 'plusoumoins', '.numpad');
+const buttons = document.querySelectorAll('button');
 const resetBtn = document.getElementById('reset');
 const clearBtn = document.getElementById('clear');
 const equalsBtn = document.getElementById('equals');
+const keys = 
+
+
+// add events
+buttons.forEach(button => {
+    button.addEventListener("click", () => {
+        input.value += button.textContent;
+    })
+});
+resetBtn.addEventListener('click', () => {
+    resetCalculator();
+})
 
 
 
