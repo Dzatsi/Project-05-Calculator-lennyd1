b20c5cc58f4154897f38031494707678c5b50d8d
lenny dzatsi <lennydzatsi8@gmail.com>
2023-08-14 06:31:57 +0200

    declaration variable

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 { calculate } import './calculator';
 
 // TODO: Faire la manipulation du DOM dans ce fichier
+// Récuperation des élement html
+
+const input = document.getElementById('input');
+const calcul = document.getElementById('calcul');
+const buttons = document.querySelectorAll('#percentage', 'plusoumoins', '.numpad');
+const resetBtn = document.getElementById('reset');
+const clearBtn = document.getElementById('clear');
+const equalsBtn = document.getElementById('equals');
+
+
+
+
