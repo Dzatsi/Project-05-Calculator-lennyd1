58d57a8f274b21dae106fe065703f42756303e89
lenny dzatsi <lennydzatsi8@gmail.com>
2023-08-15 23:40:08 +0200

    add function for many instruction

--- script.js
+++ script.js
@@ -3,108 +3,50 @@
 
 // TODO: Faire la manipulation du DOM dans ce fichier
 
-// Récuperation des élement html
+// Récuperons les élement html
 const input = document.getElementById('input');
-const calcul = document.getElementById('calcul');
-const resetBtn = document.getElementById('reset');
-const clearBtn = document.getElementById('clear');
-const equalsBtn = document.getElementById('equals');
-const plusBtn = document.getElementById('plus'); 
-const buttons = document.querySelectorAll('.numpad');
-const minusBtn = document.getElementById('minus');
-const timesBtn = document.getElementById('times');
-const dividebyBtn = document.getElementById('divideby');
-const plusmoins = document.getElementById("plusoumoins");
-const percentage = document.getElementById('percentage');
- 
-// déclaration des variables de calcul
-let btnCourant = "";
-let operCourant = "";
-let calculCourant = "";
+const buttons = document.querySelectorAll('button');
+const historiqueClc = document.querySelector('p');
 
-// ajouts des évenements aux variables stocké
+// déclaration variable pour calcul
+let calcul = '';
+let viewHist = "";
+let operateur = "";
+var verifSigne = false;
+
+// add events
 buttons.forEach(button => {
-    button.addEventListener("click", () => {
-        input.value += button.textContent;
-    })
-});
+    button.addEventListener('click', event => {
+        const btnValue = button.textContent;
 
-resetBtn.addEventListener('click', (event) =>{
-    event.preventDefault();
-    input.value = ''; 
-    calculHistoric.textContent = "";
-});
+        if (btnValue >= '0' && btnValue <= '9') {
+            if (input.value === '0' && btnValue === '0') {
+                input.value = input.value;
+                viewHist = btnValue;
+                verifSigne = false;
+            } else if(input.value === '0'){
+                input.value = btnValue;
+                viewHist = btnValue;
+            } else{
 
-clearBtn.addEventListener("click", function() {
-    if(btnCourant.length > 0){
-        btnCourant = btnCourant.slice(0, -1)
-        input.value = btnCourant;
-    }
-})
-plusmoins.addEventListener("click", function(event) {
-    event.preventDefault();
-    btnCourant = (parseFloat(btnCourant) * -1).toString();
-    input.value = btnCourant;
-    calculHistoric.textContent = btnCourant;
-});
+                if (!historiqueClc.textContent.includes('=')) {
+                    calcul += btnValue;
+                    input.value += btnValue;
+                    viewHis.value += btnValue;
+                    operateur += btnValue;
+                } else {
+                    input.value = '';
+                    viewHist = ""
+                    calcul = '';
+                    input.value += btnValue;
+                    viewHist += btnValue;
+                    calcul += btnValue;
+                    operateur += buttonValue;
+                }
+            }
 
-percentage.addEventListener("click", function(event) {
-    event.preventDefault();
-    btnCourant = (parseFloat(btnCourant) / 100).toString();
-    input.value = btnCourant
-    calculHistoric.textContent = btnCourant;
-});
 
-const dot = document.querySelector('.dot');
-dot.addEventListener('click', function() {
-    if(!btnCourant.includes('.')) {
-        btnCourant += '.';
-        input.value = btnCourant;
-    };
-})
 
-dividebyBtn.addEventListener("click", function(event) {
-    event.preventDefault();
-    calculCourant += "/";
-    calculHistoric.textContent = calculCourant;
-    input.value = "";
-    const convNmbr = calculCourant;
-    calculHistoric.textContent = convNmbr;
-});
-    
-minusBtn.addEventListener('click', function(event) {
-    event.preventDefault();
-    calculCourant += "-";
-    calculHistoric.textContent = calculCourant;
-    input.value = "";
-    const convNmbr = calculCourant;
-    calculHistoric.textContent = convNmbr;
-});
-
-plusBtn.addEventListener("click", function(event) {
-    event.preventDefault();
-    calculCourant += "+";
-    calculHistoric.textContent = calculCourant;
-    input.value = "";
-    const convNmbr = calculCourant;
-    calculHistoric.textContent = convNmbr;
-});
-    
-timesBtn.addEventListener('click', function(event) {
-    event.preventDefault();
-    calculCourant += "*";
-    calculHistoric.textContent = calculCourant;
-    input.value = "";
-    const convNmbr = calculCourant;
-    calculHistoric.textContent = convNmbr;
-});
-
-equalsBtn.addEventListener("click", function (event){
-    event.preventDefault();
-    const valueInput = eval(calculCourant);
-    input.value = valueInput;
-    calculHistoric.textContent = calculCourant + "=";
-})
 
 
 
